6d9666542b3a659eb39719aee69c436d073d4bc5
Ivan Davletshin <ivd@fb.com>
2019-08-01 17:39:13 -0400

    prettify web logger messages

--- backee/logger/loggers.py
+++ backee/logger/loggers.py
@@ -57,5 +57,4 @@ def __handle_exception(exc_type, exc_value, exc_traceback):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
 
-    log.error("Uncaught exception", exc_info=(
-        exc_type, exc_value, exc_traceback))
+    log.error(f"Uncaught exception: {exc_value}")
