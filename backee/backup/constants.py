0a5878ca2f9cbf4a46934a1868313ec48bf64769
Ivan Davletshin <ivd@fb.com>
2022-10-13 11:52:12 -0400

    do not raise errors for rsync vanished items

---
+++ backee/backup/constants.py
@@ -0,0 +1,6 @@
+# Constants for transmitter related stuff
+
+# rsync finished successfully
+RSYNC_STATUS_SUCCESS = 0
+# source item vanished before rsync was able to copy it over
+RSYNC_STATUS_SOURCE_VANISHED = 24
